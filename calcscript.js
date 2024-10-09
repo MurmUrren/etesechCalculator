dbf0fed6d746bda349b833f1bf84d5bf720e3c70
MurmUrren <igorspopovich@outlook.com>
2024-10-09 10:09:52 -0700

    Calc first working version

--- calcscript.js
+++ calcscript.js
@@ -2,7 +2,7 @@ function add(a, b){
     return a + b;
 }
 
-function substract(a, b){
+function subtract(a, b){
     return a - b;
 }
 
@@ -18,19 +18,116 @@ function divide(a, b){
 }
 
 function operate(a, operator, b) {
+    let result;
     switch (operator) {
         case '+':
-            return add(a, b);
+            result = add(a, b);
+            break;
         case '-':
-            return subtract(a, b);
+            result = subtract(a, b);
+            break;
         case '*':
-            return multiply(a, b);
+            result = multiply(a, b);
+            break;
         case '/':
-            return divide(a, b);
+            result = divide(a, b);
+            break;
         default:
             return;
     }
+    return parseFloat(result.toFixed(2));
 }
 
-let firstNumber = 0, secondNumber = 0, operator = '';
+function clear(){
+    firstNumber = '0';
+    secondNumber = '0';
+    operator = '';
+    screenInput.value = firstNumber;
+}
+
+function checkEmptyValues(){
+    if (operator === ''){
+        return true;
+    }
+    return false;
+}
+
+function emptyValues(){
+    firstNumber = '0';
+    secondNumber = '0';
+    operator = '';
+}
+
+function handleButton(e){
+    const target = e.target;
+    if (target.classList.contains('clear')){
+        clear();
+    }else if (target.classList.contains('operator')) {
+        if (firstNumber !== '0' && operator !== '') {
+            screenInput.value = operate(Number(firstNumber), operator, Number(secondNumber))
+            if (screenInput.value === 'Math error'){
+                emptyValues();
+            } else {
+                emptyValues();
+                firstNumber = String(screenInput.value);
+            }  
+        } 
+        console.log('Operator button clicked:', target.value);
+        operator = target.value;
+        console.log(typeof(operator))
+    } else if (target.classList.contains('calculate')){
+        if (!checkEmptyValues()) {
+            screenInput.value = operate(Number(firstNumber), operator, Number(secondNumber));
+            if (screenInput.value === 'Math error'){
+                emptyValues();
+            } else {
+                emptyValues();
+                firstNumber = String(screenInput.value);
+            }  
+        }
+    }else if (operator === '') {
+        console.log('First numbers clicked:', target.value);
+        if (firstNumber === '0') {
+            if (target.classList.contains('decimal') && !firstNumber.includes('.')) {
+                firstNumber += target.value;
+            } else {
+                firstNumber = target.value;
+            }
+        } else if ((target.classList.contains('decimal') && !firstNumber.includes('.'))) {
+            firstNumber += target.value;
+        }  else if (firstNumber.includes('.') && !target.classList.contains('decimal')){
+            firstNumber += target.value;
+        } else if (!target.classList.contains('decimal')) {
+            firstNumber += target.value;
+        }
+        screenInput.value = firstNumber;
+    } else { 
+        console.log('Second numbers clicked:', target.value);
+        if (secondNumber === '0') {
+            if (target.classList.contains('decimal') && !secondNumber.includes('.')) {
+                secondNumber += target.value;
+            } else {
+                secondNumber = target.value;
+            }
+        } else if ((target.classList.contains('decimal') && !secondNumber.includes('.'))) {
+            secondNumber += target.value;
+        }  else if (secondNumber.includes('.') && !target.classList.contains('decimal')){
+            secondNumber += target.value;
+        } else if (!target.classList.contains('decimal')){
+            secondNumber += target.value;
+        }
+        screenInput.value = secondNumber;
+    }
+}
+
+
+let firstNumber = '0', secondNumber = '0', operator = '';
+
+let numButtons = document.querySelectorAll(".btn");
+
+numButtons.forEach(button => {
+    button.addEventListener("click", handleButton);
+});
+
+let screenInput = document.querySelector("input");
 
